630e05c530b20726b0d9ed33b419587eae7d8294
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-09 16:58:33 -0600

    Added test class for Library class

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,4 +1,18 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+    @Before
+    public void initializeClass() throws Exception {
+       library = new Library();
+       library.stock();
+    }
+    @Test
+    public void listBooksTest() {
+        assertEquals(0, 1);
+    }
 }
