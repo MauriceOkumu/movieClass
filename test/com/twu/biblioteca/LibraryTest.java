dd886a017a9beb75a1fdaefdcbccb2f9b2e76590
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-09 19:04:20 -0600

    Library class tests passing

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -9,10 +9,22 @@ public class LibraryTest {
     @Before
     public void initializeClass() throws Exception {
        library = new Library();
-       library.stock();
+       library.initiate();
     }
     @Test
     public void listBooksTest() {
-        assertEquals(0, 1);
+        assertEquals(library.getMovie().size(), 9);
+    }
+    @Test
+    public void lookTitle() {
+        assertEquals(library.getMovie().get(0).getYear(), 2009);
+    }
+    @Test
+    public void checkStars() {
+        assertEquals(library.getMovie().get(7).getRating(),"6 stars");
+    }
+    @Test
+    public void checkAnotherStar() {
+        assertEquals(library.getMovie().get(6).getRating(),"10 stars");
     }
 }
