1beab2c2d2a1b8bba7eff751e3082342e54ce521
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-09 20:43:29 -0600

    Added User class tests

--- test/com/twu/biblioteca/UserClassTest.java
+++ test/com/twu/biblioteca/UserClassTest.java
@@ -1,4 +1,18 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class UserClassTest {
+    UserClass user;
+    @Before
+    public void initializeClass() {
+         user = new UserClass("Maurice");
+
+    }
+    @Test
+    public void checkUserName() {
+        assertEquals(user.getName(), "Maurice");
+    }
 }
