467ca68fdbac2c5f6be02b552e73421f35135561
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-10 18:32:37 -0600

    All tests for the class failing at the moment--refactor

--- test/com/twu/biblioteca/UserinputcaptureTest.java
+++ test/com/twu/biblioteca/UserinputcaptureTest.java
@@ -1,4 +1,19 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class UserinputcaptureTest {
+    private UserClass user;
+    //@Before
+//    public void initializeClass() {
+//        user = new UserClass(234567789,"Maurice","a.m@yahoo.com","abc-defg","234567789");
+//
+//    }
+//    @Test
+//    public void checkPassword() {
+//        assertEquals(user.getPassword().indexOf("-"),3);
+//    }
+
 }
