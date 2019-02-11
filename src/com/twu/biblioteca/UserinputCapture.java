3c5bc3dc47df946b7d5c1678013ed7e6988d366e
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-10 18:31:39 -0600

    Made class reusable

--- src/com/twu/biblioteca/UserinputCapture.java
+++ src/com/twu/biblioteca/UserinputCapture.java
@@ -1,4 +1,28 @@
 package com.twu.biblioteca;
 
 public class UserinputCapture {
+    public String libNumber;
+    public String name;
+    public String email;
+    public String password;
+    public String phoneNumber;
+    ScannerClass captureInfo;
+    public UserinputCapture() {
+
+    }
+        public void UserinputCaptureRun () {
+            captureInfo = new ScannerClass();
+            System.out.print("What is you Library number? :");
+            libNumber = captureInfo.scan();
+            System.out.print("What is your name ? :");
+            name = captureInfo.scan();
+            System.out.print("Put your email address => :");
+            email = captureInfo.scan();
+            System.out.print("What is your password ? :");
+            password = captureInfo.scan();
+            System.out.print("Phone number :");
+            phoneNumber = captureInfo.scan();
+        }
+
+
 }
