f48bc9c69c71622b2a950269e2d43a811ad81deb
Maurice Okumu <mauriceokumu@Maurices-MacBook-Pro.local>
2019-02-10 20:56:10 -0600

    Added test stubs

--- test/com/twu/biblioteca/LibrarianClassTest.java
+++ test/com/twu/biblioteca/LibrarianClassTest.java
@@ -1,4 +1,22 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class LibrarianClassTest {
+    private Librarian librarian;
+    @Before
+    public void initializeClass() {
+        librarian = new Librarian();
+        librarian.addUserWhoBorrowedBook();
+    }
+    @Test
+    public void checkPage() {
+        assertEquals(librarian.getUsersWoHaveBooks().get(0),"Maurice");
+    }
+    @Test
+    public void checkListOfBorrowers() {
+        assertEquals(librarian.getUsersWoHaveBooks().size(), 1);
+    }
 }
